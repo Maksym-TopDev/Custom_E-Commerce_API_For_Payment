e355d2ccece2373cbeac7536661ee5475e452dc9
Mr-waweru <gitaugeorge001@gmail.com>
2025-02-14 14:58:18 +0300

    Register Product and StockLog models in admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import StockLog, Product
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "stock", "category")
+    search_fields = ("name", "category")
+    list_filter = ()
+    ordering = ("-id",)
+    readonly_fields = ()
+
+admin.site.register(Product, ProductAdmin)
+
+
+class StockLogAdmin(admin.ModelAdmin):
+    list_display = ("id", "product", "change_type", "quantity_changed", "new_stock_level", "timestamp")
+    list_filter = ("change_type", "timestamp")
+    search_fields = ("product__name", "change_type")
+    ordering = ("-timestamp",)  # Show newest changes first
+
+admin.site.register(StockLog, StockLogAdmin)
