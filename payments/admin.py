4dae8001cabc5d8cd1086fa316417f04640b3f97
Mr-waweru <gitaugeorge001@gmail.com>
2025-02-14 15:46:22 +0300

    Register Payment model in admin panel

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Payment
 
 # Register your models here.
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ("id", "order", "amount", "status", "stripe_payment_intent")
+    search_fields = ("order__id", "stripe_payment_intent", "status")
+    list_filter = ("status",)
+    ordering = ("-id",)
+    readonly_fields = ()
+
+admin.site.register(Payment, PaymentAdmin)
