58f934b90b7e2ea037efc7c54fbf92d15ffc6a4a
Mr-waweru <gitaugeorge001@gmail.com>
2025-02-14 14:44:57 +0300

    Register CustomUser model with admin panel customization

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 # Register your models here.
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("id", "email", "username", "first_name", "last_name", "is_active", "is_staff", "date_joined")
+    search_fields = ("email", "username", "first_name", "last_name")
+    list_filter = ("is_active", "is_staff", "date_joined")
+    ordering = ("-date_joined",)
+    readonly_fields = ("date_joined", "last_login")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal Info", {"fields": ("username", "first_name", "last_name")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important Dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
