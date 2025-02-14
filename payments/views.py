c5e05eab5cc26c0aaf9b590cf5431d0b7035abc0
Mr-waweru <gitaugeorge001@gmail.com>
2025-02-14 15:49:25 +0300

    Create API views for listing and managing payments

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+import stripe
+from django.conf import settings
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from .models import Payment
+from .serializers import PaymentSerializer
+from orders.models import Order
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 # Create your views here.
+class PaymentListCreateView(generics.ListCreateAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        """
+        Ensures the payment is linked to the requesting user.
+        """
+        serializer.save(user=self.request.user)
+
+
+class PaymentDetailView(generics.RetrieveUpdateAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = [permissions.IsAuthenticated]
