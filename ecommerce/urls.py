6433743e5696e552a11ea096309f5e9b884f2ed4
Mr-waweru <gitaugeorge001@gmail.com>
2025-03-06 01:15:24 +0300

    Add register and login of users

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from accounts import views
+from django.contrib.auth import views as auth_views
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -12,26 +14,40 @@ from rest_framework_simplejwt.views import (
 # from drf-yasg documentation
 schema_view = get_schema_view(
    openapi.Info(
-      title="Blogging Platform API",
+      title="A Custom E-Commerce API",
       default_version='v1',
-      description="A DRF API for blogging that allows users to create and comment on different posts.\nUser can also view most liked post or a post with the highest rating.\nA post can also be shared via email",
-      contact=openapi.Contact(email="george@gmail.com"),
+      description="A scalable and secure API for customizable e-commerce solutions tailored for small businesses. \nSupports for coupon codes and payment integration (Stripe) and m-pesa. \nSome of the features include real-time inventory updates, webhook-driven payment status tracking, and optimized API performance for seamless order management.",
+      terms_of_service="https://www.example.com/terms/",
+      contact=openapi.Contact(email=""),
+      license=openapi.License(name="MIT License"),
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
+    path("register/", views.register, name="register" ),
+    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
+
+
+    # Protected endpoints for logged in users
     path('admin/', admin.site.urls),
-    path("users/", include("users.urls")),
-    path("products/", include("products.urls")),
-    path("orders/", include("orders.urls")),
-    path("payments/", include("paymentsurls")),
+    path("", include("users.urls")),
+    path("", include("products.urls")),
+    path("", include("orders.urls")),
+    path("", include("payments.urls")),
 
     # token generation url
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # Documentaing using drf-yasg
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
