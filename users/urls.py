b0935df922e3f90b4de15a830cd189f7a314a587
Mr-waweru <gitaugeorge001@gmail.com>
2025-03-06 01:18:33 +0300

    Remove register url

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("register/", views.UserRegisterView.as_view(), name="user-register"),
     path("profile/", views.UserDetailView.as_view(), name="user-detail"),
 ]
-
-# POST /register/ → User registration
-# GET/PUT /profile/ → View & update user profile
