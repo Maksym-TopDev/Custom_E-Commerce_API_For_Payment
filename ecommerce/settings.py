5f580fb668053cfb44c7c8b943996b0f215add54
Mr-waweru <gitaugeorge001@gmail.com>
2025-03-05 19:42:57 +0300

    Add mpesa credentials API settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -1,6 +1,10 @@
 from pathlib import Path
+import os
 import environ
 from datetime import timedelta
+import rest_framework
+from decouple import config
+from decimal import Decimal
 
 # Initialize environment variables
 env = environ.Env(
@@ -24,8 +28,26 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = env("DEBUG")
 
 STRIPE_SECRET_KEY = env("STRIPE_SECRET_KEY")
+STRIPE_WEBHOOK_SECRET = env("STRIPE_WEBHOOK_SECRET")
 
-ALLOWED_HOSTS = []
+# Mpesa credentials
+MPESA_ENV = env("MPESA_ENV", default="sandbox")
+MPESA_CONSUMER_KEY = env("MPESA_CONSUMER_KEY")
+MPESA_CONSUMER_SECRET = env("MPESA_CONSUMER_SECRET")
+
+if MPESA_ENV == "sandbox":
+    MPESA_SHORTCODE = env("MPESA_SHORTCODE", default="174379")
+    MPESA_PASSKEY = env("MPESA_PASSKEY", default="bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919")
+else:
+    MPESA_SHORTCODE = env("MPESA_SHORTCODE")
+    MPESA_PASSKEY = env("MPESA_PASSKEY")
+
+MPESA_CALLBACK_URL = env("MPESA_CALLBACK_URL", default="https://yourdomain.com/api/payments/mpesa-callback/")
+
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
@@ -39,6 +61,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Apps
+    "accounts",
     "products",
     "orders",
     "users",
@@ -47,9 +70,13 @@ INSTALLED_APPS = [
     # Third party apps
     "rest_framework",
     "rest_framework_simplejwt",
+    "drf_yasg",
+    "django_filters",
     "corsheaders",
-
+    "markdownx",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True  # Allow all origins (for testing)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -66,7 +93,7 @@ ROOT_URLCONF = 'ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -78,6 +105,10 @@ TEMPLATES = [
         },
     },
 ]
+
+# URL to redirect after login and for login-required pages
+LOGIN_REDIRECT_URL = '/orders/'
+LOGIN_URL = '/login/'
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
@@ -128,6 +159,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = "/media/"
+
+# Set the static root directory (for `collectstatic`)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # This is where static files will be collected
+
+STATICFILES_DIRS = [
+    os.path.join(os.path.dirname(rest_framework.__file__), "static"),
+]
+
+# Media files (for uploaded files)
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -139,11 +181,26 @@ AUTH_USER_MODEL = "users.CustomUser"
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAdminUser',
     ],
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    
 }
+
+if os.environ.get('DJANGO_ENV') == 'production':
+    REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.OpenAPIRenderer',
+    )
+else:
+    REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.OpenAPIRenderer',
+    )
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),  
@@ -153,11 +210,16 @@ SIMPLE_JWT = {
 }
 
 SWAGGER_SETTINGS = {
-   'SECURITY_DEFINITIONS': {
-      'Bearer': {
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
             'type': 'apiKey',
             'name': 'Authorization',
-            'in': 'header'
-      }
-   }
+            'in': 'header',
+        }
+    },
+    'USE_SESSION_AUTH': False,
+    'DEFAULT_MODEL_RENDERING': 'example',
+    'DEFAULT_FIELD_INSPECTORS': [
+        'utils.inspectors.DecimalAsFloatInspector',  # Custom inspector for Decimal fields
+    ],
 }
