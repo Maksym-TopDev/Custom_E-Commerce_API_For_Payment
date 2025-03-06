548e227a5d00a20c4dbef2303e6be92a908bbbd8
Mr-waweru <gitaugeorge001@gmail.com>
2025-03-07 01:34:09 +0300

    Define API endpoint for mpesa

--- payments/urls.py
+++ payments/urls.py
@@ -6,8 +6,5 @@ urlpatterns = [
     path("payments/", views.PaymentListCreateView.as_view(), name="payment-list"),
     path("payments/<int:pk>/", views.PaymentDetailView.as_view(), name="payment-detail"),
     path("webhook/", stripe_webhook, name="stripe-webhook"),
+    path("mpesa-pay/", views.MpesaPaymentView.as_view(), name="mpesa-pay"),
 ]
-
-"""Routes:"""
-    # - GET/POST /payments/ → List & create payments
-    # - GET/PUT /payments/<id>/ → View & update payment details
