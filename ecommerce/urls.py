32dae7fbb444bd4668569ec379a8f43a9074eaa1
Mr-waweru <gitaugeorge001@gmail.com>
2025-03-07 16:31:02 +0300

    Append api/ to all urls

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,7 +16,7 @@ schema_view = get_schema_view(
    openapi.Info(
       title="A Custom E-Commerce API",
       default_version='v1',
-      description="A scalable and secure API for customizable e-commerce solutions tailored for small businesses. \nSupports for coupon codes and payment integration (Stripe) and m-pesa. \nSome of the features include real-time inventory updates, webhook-driven payment status tracking, and optimized API performance for seamless order management.",
+      description="A scalable and secure API for customizable e-commerce solutions tailored for small businesses. \nSupports for coupon codes and payment integration Stripe and m-pesa. \nSome of the features include real-time inventory updates, webhook-driven payment status tracking, and optimized API performance for seamless order management.",
       terms_of_service="https://www.example.com/terms/",
       contact=openapi.Contact(email=""),
       license=openapi.License(name="MIT License"),
@@ -26,21 +26,21 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("register/", views.register, name="register" ),
-    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
+    path("api/register/", views.register, name="register" ),
+    path("api/login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("api/logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
 
 
     # Protected endpoints for logged in users
     path('admin/', admin.site.urls),
-    path("", include("users.urls")),
-    path("", include("products.urls")),
-    path("", include("orders.urls")),
-    path("", include("payments.urls")),
+    path("api/", include("users.urls")),
+    path("api/", include("products.urls")),
+    path("api/", include("orders.urls")),
+    path("api/", include("payments.urls")),
 
     # token generation url
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # Documentaing using drf-yasg
     path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
