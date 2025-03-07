caa798fe9c58078dd465558558e2877033bad280
Mr-waweru <gitaugeorge001@gmail.com>
2025-03-07 16:34:49 +0300

    Remove routes examples of products urls

--- products/urls.py
+++ products/urls.py
@@ -8,10 +8,3 @@ urlpatterns = [
     path("products/<int:pk>/", views.ProductDetailView.as_view(), name="product-detail"),
     path("stock-logs/", views.StockLogListView.as_view(), name="stock-log-list"),
 ]
-
-"""Routes:"""
-    # - GET/POST /categories/ → List & create categories
-    # - GET/PUT/DELETE /categories/<id>/ → Manage a category
-    # - GET/POST /products/ → List & create products
-    # - GET/PUT/DELETE /products/<id>/ → Manage a product
-    # - GET /stock-logs/ → View stock log history
